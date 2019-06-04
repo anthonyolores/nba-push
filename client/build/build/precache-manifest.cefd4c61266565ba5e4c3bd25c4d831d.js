d8e9a4363571e0b1c1591569e539cc60b7275dd4
Anthony Olores <anthony@ABL.local>
2019-06-05 09:53:52 +1200

    adjusted button

---
+++ client/build/build/precache-manifest.cefd4c61266565ba5e4c3bd25c4d831d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adfb8932d0ee782d6e455b8d11abe292",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fd5d8c771c6d7d595e6",
+    "url": "/static/css/2.c7bb94bc.chunk.css"
+  },
+  {
+    "revision": "0f762158f66ae923346f",
+    "url": "/static/css/main.ddcda2ec.chunk.css"
+  },
+  {
+    "revision": "6fd5d8c771c6d7d595e6",
+    "url": "/static/js/2.24662e0b.chunk.js"
+  },
+  {
+    "revision": "0f762158f66ae923346f",
+    "url": "/static/js/main.496a0614.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ec8c9be4f51b916b65dd7702d95afd59",
+    "url": "/static/media/logo.ec8c9be4.png"
+  }
+]);
---
+++ client/build/build/service-worker.js
@@ -0,0 +1,46 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.cefd4c61266565ba5e4c3bd25c4d831d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+console.log('Service Worker Loaded');
+self.addEventListener('push', e => {
+    const data = e.data.json();
+    console.log('Push notification has been received!');
+    self.registration.showNotification(data.title,{
+        body: data.message,
+    });
+});
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
